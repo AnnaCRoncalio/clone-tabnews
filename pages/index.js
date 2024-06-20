9cea163354b492c6630ed7e015a25655cea6d3a1
Anna Cecília Roncalio <101569495+AnnaCRoncalio@users.noreply.github.com>
2024-06-20 11:25:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olha aqui eu colocando um site no ar apenas para você 🖤</h1>;
-};
+  return <h1>Olha aqui eu colocando um site no ar apenas para você 🖤</h1>;
+}
 
 export default Home;
