ca38d212578e3d9e340fb3c97c72b8da2267c851
Anna Cecília Roncalio <101569495+AnnaCRoncalio@users.noreply.github.com>
2024-07-20 19:38:15 +0000

    init staging

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -35,5 +35,9 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
+  if (request.method === "DELETE") {
+    return [];
+  }
+
   return response.status(405).end();
 }
